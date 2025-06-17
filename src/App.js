e0833785f86f738c1daa0460100d8e99d318cac1
rex1008 <zhangrui.coder@gmail.com>
2025-06-17 21:33:10 +0800

    redux init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { memo } from 'react'
 import { HashRouter } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
+import { Provider } from 'react-redux'
 
 
 import routes from './router'
+import store from './store'
 
 import MCAppHeader from './components/app-header'
 import MCAppFooter from './components/app-footer'
 
 export default memo(function App() {
   return (
-    <HashRouter>
-      <MCAppHeader/>
-      {renderRoutes(routes)}
-      <MCAppFooter/>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <MCAppHeader/>
+        {renderRoutes(routes)}
+        <MCAppFooter/>
+      </HashRouter>
+    </Provider>
   )
 })
